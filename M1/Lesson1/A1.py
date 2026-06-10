292d5315d28cf253fd01fc11cb48eee98ac86c53
Abhishek Gupta <107456489+AbhishekGupta1212@users.noreply.github.com>
2026-06-10 11:11:10 +0530

    M1L1

---
+++ M1/Lesson1/A1.py
@@ -0,0 +1,14 @@
+#Print a Sentence 
+print("Welcome to bambarbola land")
+
+#Print a Number
+print(3)
+
+#Next Line after print
+print("Hello Everyone \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")
